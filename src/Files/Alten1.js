3fa49a2efd1f7edd581e0cd82e8621fe323b6240
Abdelmouhaimen sarhane <68982208+Abdelmouhaimen@users.noreply.github.com>
2024-07-05 19:47:56 +0200

    Update Alten1.js

--- src/Files/Alten1.js
+++ src/Files/Alten1.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/Files/lette_de_motivation_alten.pdf";
+import pdf from "../Assets/Files/lette_de_motivation_alten.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
